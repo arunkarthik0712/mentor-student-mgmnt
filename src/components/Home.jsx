3f0ceae98b23ad43b3dad30f964dc54c1c765399
arunkarthik0712 <arunkarthik0710@gmail.com>
2024-07-26 11:16:43 +0530

    components

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="container mt-5">
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome to the Mentor-Student Portal</h1>
+        <p className="lead">Manage mentors and students effortlessly.</p>
+        <hr className="my-4" />
+        <p>
+          Navigate through the menu to create and assign mentors and students.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
